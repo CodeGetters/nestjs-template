a96cdf3978de37dcc8dd8b6c3b9c276e6ce734f7
Simon <2775145275@qq.com>
2024-12-10 19:26:27 +0800

    feat: :sparkles: 修改pm2 配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,6 @@ module.exports = {
       name: 'server',
       // 入口文件路径
       script: 'dist/main.js',
-      env: {
-        DATABASE_URL:
-          'postgresql://postgres.zwzuumzunlpzbxmpdiem:3VJxgKQzKXKF8xUVB1iV@aws-0-us-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true',
-        DIRECT_URL:
-          'postgresql://postgres.zwzuumzunlpzbxmpdiem:3VJxgKQzKXKF8xUVB1iV@aws-0-us-west-1.pooler.supabase.com:5432/postgres',
-      },
       env_production: {
         NODE_ENV: 'production',
       },
