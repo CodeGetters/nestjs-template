a8d6bbd9bceed099159ad72c9a4606e29b85e23a
Simon <2775145275@qq.com>
2025-01-18 22:51:27 +0800

    chore: ♻️ 移除@开头的引用...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 // import { LoggerModule } from './common/logger/logger.module';
 // import { PrismaModule } from './common/prisma/prisma.module';
-import { AllExceptionFilter } from '@/core/filter/all-exception.filter';
-import { HttpExceptionFilter } from '@/core/filter/http-exception.filter';
-// import { V1Module } from './api/index';
+// import { AllExceptionFilter } from '@/core/filter/all-exception.filter';
+// import { HttpExceptionFilter } from '@/core/filter/http-exception.filter';
+import { V1Module } from './api/index';
 
-import { APP_FILTER } from '@nestjs/core';
+// import { APP_FILTER } from '@nestjs/core';
 const NODE_ENV =
   process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
@@ -13,16 +13,17 @@ console.log('=========NODE_ENV==========', NODE_ENV);
 
 @Module({
   // imports: [PrismaModule, LoggerModule, V1Module],
-  providers: [
-    // 执行顺序：从后往前！
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionFilter,
-    },
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  imports: [V1Module],
+  // providers: [
+  //   // 执行顺序：从后往前！
+  //   {
+  //     provide: APP_FILTER,
+  //     useClass: AllExceptionFilter,
+  //   },
+  //   {
+  //     provide: APP_FILTER,
+  //     useClass: HttpExceptionFilter,
+  //   },
+  // ],
 })
 export class AppModule {}
