3f3893bc30385e41b16768c24998a65a28d3b8c1
Simon <2775145275@qq.com>
2025-01-18 22:11:45 +0800

    build: 🛠 增加vercel配置文件

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import fastifyStatic from '@fastify/static';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { TransformInterceptor } from '@/core/interceptor/transform.interceptor';
 import { TimeoutInterceptor } from '@/core/interceptor/timeout.interceptor';
@@ -40,7 +43,41 @@ async function bootstrap() {
   // 响应时间超过3s为超时错误
   app.useGlobalInterceptors(new TimeoutInterceptor(60 * 1000));
 
-  await app.listen(8080, '0.0.0.0');
+  // 添加静态文件服务
+  await app.register(fastifyStatic as any, {
+    root: join(process.cwd(), 'public'),
+    prefix: '/',
+    decorateReply: false, // 避免多个静态文件插件之间的装饰器冲突
+    schemaHide: true, // 隐藏该路由在 Swagger 中的显示
+    serve: false, // 禁用自动文件服务
+  });
+
+  app
+    .getHttpAdapter()
+    .getInstance()
+    .addHook('onRequest', (req, reply, done) => {
+      // 处理根路径和 index.html 请求
+      if (req.url === '/' || req.url === '/index.html') {
+        const filePath = join(process.cwd(), 'public', 'index.html');
+        try {
+          const html = readFileSync(filePath, 'utf-8');
+          reply
+            .status(200)
+            .header('Content-Type', 'text/html; charset=utf-8')
+            .send(html);
+        } catch (error) {
+          console.log('error', error);
+          reply
+            .status(500)
+            .header('Content-Type', 'text/plain; charset=utf-8')
+            .send('Internal Server Error');
+        }
+        done();
+        return;
+      }
+    });
+
+  await app.listen(8888, '0.0.0.0');
   console.log(`Server is running on: ${await app.getUrl()}`);
   console.log(`Docs is running on: ${await app.getUrl()}/docs`);
   console.log(`Demo API is running on: ${await app.getUrl()}/api/v1/demo`);
